c3b5c0663590062f05b3ee1ef523bdb0d57cb5c8
Mayank <mayankkanti2325@gmail.com>
2025-04-24 14:27:04 +0530

    Initial commit

---
+++ src/main/java/net/bytexo/weathermod/WeatherMod.java
@@ -0,0 +1,17 @@
+package net.bytexo.weathermod;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WeatherMod implements ClientModInitializer {
+	public static final String MOD_ID = "weatherstatus";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("Weather Status Mod Initialized!");
+	}
+}
---
+++ src/main/java/net/bytexo/weathermod/WeatherModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bytexo.weathermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WeatherModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
